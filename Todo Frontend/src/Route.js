2aaa48926e33aef837b68126e951304068278387
Naman0901 <73291949+Naman0901@users.noreply.github.com>
2023-01-20 18:00:51 +0530

    Add files via upload

---
+++ Todo Frontend/src/Route.js
@@ -0,0 +1,10 @@
+import ToDos from "./components/ToDos.vue";
+import Addto from "./components/Addtodo.vue";
+import Login from "./components/login.vue";
+
+
+export default[
+    {path: '/', component: Login},
+    {path: '/add', component: Addto}
+]
+
